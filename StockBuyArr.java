084564caf742e54fedaa261b04c19b53f8dcdcb2
Ali khan <khanali6861@gmail.com>
2025-01-10 15:25:41 +0530

    matrix

---
+++ StockBuyArr.java
@@ -0,0 +1,18 @@
+public class StockBuyArr {
+    public static void main(String[] args) {
+        int arr[]={7,1,3,5,6};
+        int minprice=Integer.MAX_VALUE;
+        int maxprofit=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<minprice){
+                minprice=arr[i];
+            }
+            int profit=arr[i]-minprice;
+            if(profit>maxprofit){
+                maxprofit=profit;
+            }
+        }
+        System.out.println("Maximum profit is "+maxprofit);
+
+    }
+}
