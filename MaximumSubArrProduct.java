4a4402d6d3d98aea1215b8c7618146a52f8b74df
Ali khan <khanali6861@gmail.com>
2025-02-10 19:11:47 +0530

    RGU

--- MaximumSubArrProduct.java
+++ MaximumSubArrProduct.java
@@ -1,14 +1,14 @@
 public class MaximumSubArrProduct {
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,0};
-        int maxProduct=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            int product=1;
-            for(int j=i;j<arr.length;j++){
-               product=product*arr[j];
-               maxProduct=Math.max(maxProduct,product);
-            }
+        int arr[]={1,2,3,4,5,0,1};
+      int maxProduct=Integer.MIN_VALUE;
+      for(int i=0;i<arr.length;i++){
+        int product=1;
+        for(int j=i;j<arr.length;j++){
+            product=product*arr[j];
+            maxProduct=Math.max(maxProduct, product);
         }
+      }
         System.out.println("The maximum sub array product is "+maxProduct);
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,11 +1,14 @@
- // for (int num : arr) {
-        //     if (count == 0) {
-        //         candidate = num;  // Pick a new candidate when count drops to 0
-        //     }
-        //     if (num == candidate) {
-        //         count++;  // Increase count if the number equals the candidate
-        //     } else {
-        //         count--;  // Otherwise, decrease count
-        //     }
-        // }
-        
+public class MaximumSubArrProduct {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,0,1};
+      int maxProduct=Integer.MIN_VALUE;
+      for(int i=0;i<arr.length;i++){
+        int product=1;
+        for(int j=i;j<arr.length;j++){
+            product=product*arr[j];
+            maxProduct=Math.max(maxProduct, product);
+        }
+      }
+        System.out.println("The maximum sub array product is "+maxProduct);
+    }
+}
