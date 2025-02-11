2c9dfc9cb2928f3ee9831eaa8c27520458a522c1
Ali khan <khanali6861@gmail.com>
2025-02-11 17:19:43 +0530

    Floor Ceil

--- FloorinSortArr.java
+++ FloorinSortArr.java
@@ -4,18 +4,17 @@ public class FloorinSortArr {
         int k=6;
         int floor=-1;
         int start=0,end=arr.length-1;
-        while (start<=end) {
+        while(start<=end){
             int mid=(start+end)/2;
             if(arr[mid]==k){
                 floor=arr[mid];
-                break;
+                return;
             }else if(arr[mid]<k){
                 floor=arr[mid];
                 start=mid+1;
             }else{
-                end=mid-1;
+            end=mid-1;
             }
-            
         }
         System.out.println("The floor for value: "+k+" is "+floor);
     }
